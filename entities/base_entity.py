4b3b699f0d949bba53eb42777c139f6067c91cd6
Vladislav Spasov <wladislawspassow@gmail.com>
2020-08-31 22:00:39 +0300

    Add enemy.py and inits

--- entities/base_entity.py
+++ entities/base_entity.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from helpers_and_utilities.verfication_mixin import VerificationMixin
+from helpers_and_utilities.verification_mixin import VerificationMixin
 
 
 class BaseEntity(ABC, VerificationMixin):
---
+++ main.py
@@ -0,0 +1,9 @@
+from entities.hero import Hero
+
+
+def main():
+    hero = Hero.create_hero()
+
+
+if __name__ == '__main__':
+    main()
